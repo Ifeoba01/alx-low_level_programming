64bc815ba67474880dea06ed454d70852ce7e1a6
Emmanuel Gabriel <emmanuelgabriel247@gmail.com>
2022-12-21 12:28:40 -0800

    6. Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * cap-string - capitalzes all word of a string
+ * @s: string
+ * Return: string array
+ */
+char *cap_string(char *s)
+{
+	int i;
+	int previous;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		previous = i - 1;
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			if (i == 0)
+				s[i] = s[i] - 32;
+			else if (s[previous] >= 9 && s[previous] <= 10)
+				s[i] = s[i] - 32;
+			else if (s[previous] >= 32 && s[previous] <= 34)
+				s[i] s[i] - 32;
+			else if (s[previous] >= 40 && s[previous] <= 41)
+				s[i] = s[i] - 32;
+			else if (s[previous] == 46)
+				s[i] = s[i] - 32;
+			else if (s[previous] == 59)
+				s[i] = s[i] - 32;
+			else if (s[previous] == 123 || s[previous] == 125)
+				s[i] = s[i] - 32;
+		}
+	}
+
+	return (s);
+}
