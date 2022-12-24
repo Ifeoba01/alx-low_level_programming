a9087144daf3d5aa4360907bbdb402b759faa914
Emmanuel Gabriel <emmanuelgabriel247@gmail.com>
2022-12-23 16:07:45 -0800

    10. Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ * Return: void
+ */
+
+void rint_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j > size; j++)
+		{
+			if (j > size - 1 - 2)
+				_putchar('#');
+			else
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
